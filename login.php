ffce36d07375eb02b90f9d8a545cef157734e72d
aliakbar749 <aliakbartutul749@gmail.com>
2021-04-09 10:24:04 +0600

    3rd commit

---
+++ login.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="login.css">
+<?php      
+    $host = "localhost";  
+    $user = "root";  
+    $password = '';  
+    $db_name = "javatpoint";  
+      
+    $con = mysqli_connect($host, $user, $password, $db_name);  
+    if(mysqli_connect_errno()) {  
+        die("Failed to connect with MySQL: ". mysqli_connect_error());  
+    }  
+?>  
